ccfa3cb9ecdabf183de0103bf9c9f3ca0a61252e
gricher-crto <{actor}@users.noreply.github.com>
2025-07-03 17:01:55 +0000

    [250703] Automatic update of SDK.

--- sdks/marketingsolutions_2024-04/src/main/java/com/criteo/api/marketingsolutions/v2024_04/ApiClient.java
+++ sdks/marketingsolutions_2024-04/src/main/java/com/criteo/api/marketingsolutions/v2024_04/ApiClient.java
@@ -213,7 +213,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2024.04.0.250626/java");
+        setUserAgent("OpenAPI-Generator/2024.04.0.250703/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdks/marketingsolutions_2024-10/src/main/java/com/criteo/api/marketingsolutions/v2024_10/ApiClient.java
+++ sdks/marketingsolutions_2024-10/src/main/java/com/criteo/api/marketingsolutions/v2024_10/ApiClient.java
@@ -213,7 +213,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2024.10.0.250626/java");
+        setUserAgent("OpenAPI-Generator/2024.10.0.250703/java");
 
         authentications = new HashMap<String, Authentication>();
     }
