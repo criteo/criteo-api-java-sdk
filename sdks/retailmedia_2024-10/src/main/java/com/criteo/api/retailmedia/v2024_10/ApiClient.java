93bf8505fe4c2e33366b4f6dfb56dd6565e5d7cc
gturri <{actor}@users.noreply.github.com>
2025-09-18 08:50:35 +0000

    [250918] Automatic update of SDK.

--- sdks/retailmedia_2024-10/src/main/java/com/criteo/api/retailmedia/v2024_10/ApiClient.java
+++ sdks/retailmedia_2024-10/src/main/java/com/criteo/api/retailmedia/v2024_10/ApiClient.java
@@ -213,7 +213,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2024.10.0.250915/java");
+        setUserAgent("OpenAPI-Generator/2024.10.0.250918/java");
 
         authentications = new HashMap<String, Authentication>();
     }
